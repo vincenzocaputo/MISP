376e1ebb56bd8830bbcf2ca5886ced94f2a295af
iglocska <andras.iklody@gmail.com>
2024-11-19 07:45:40 +0100

    chg: [pymisp] version bump to match 2.5.x

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -34,7 +34,7 @@ class AppController extends Controller
     public $helpers = array('OrgImg', 'FontAwesome', 'UserName');
 
     private $__queryVersion = '167';
-    public $pyMispVersion = '2.4.198';
+    public $pyMispVersion = '2.5.2';
     public $phpmin = '7.2';
     public $phprec = '7.4';
     public $phptoonew = '8.0';
